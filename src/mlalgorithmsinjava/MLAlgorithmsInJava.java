f11d6a5b70b377c970e7e3c7c691ad063f9eca3f
garciart <rgarcia@rgprogramming.com>
2019-01-27 12:18:51 -0500

    Replace thetaMatrix loop with convergenceFlag. Fixed multi-dimensional arrays and finished gradient descent residuals. Need to compare to Octave results.

--- src/mlalgorithmsinjava/MLAlgorithmsInJava.java
+++ src/mlalgorithmsinjava/MLAlgorithmsInJava.java
@@ -40,9 +40,9 @@ public class MLAlgorithmsInJava {
      * x[1][n] must always equal 1 to allow the formula to return theta[0][n] (the y-intercept for all lines of x).
      * There must be a row of x values in the x matrix for each element in the y vector.
      */
-    public static Double[][] xMatrix = {{1.0, 1.0, 2.0},
-                                        {1.0, 2.0, 3.0},
-                                        {1.0, 3.0, 4.0}
+    public static Double[][] xMatrix = {{1.0, 1.0},
+                                        {1.0, 2.0},
+                                        {1.0, 3.0}
     };
     
     /**
@@ -58,9 +58,7 @@ public class MLAlgorithmsInJava {
      * The number of elements in each row of theta must match the number elements in each row of x.
      * theta[0][n] is equal to the y-intercept of the prediction.
      */
-    public static Double[][] thetaMatrix = {{0.0, 1.0, 1.0},
-                                            {1.0, 0.0, 1.0}
-    };
+    public static Double[] thetaVector = {0.0, 0.5};
     
     /**
      * @param args the command line arguments
@@ -74,33 +72,31 @@ public class MLAlgorithmsInJava {
             exit(0);
         }
         // Check that there is a theta value for each value of x (per row). 
-        else if(xMatrix[0].length != thetaMatrix[0].length) {
-            System.out.println("Check your data: the number of x elements (" + xMatrix[0].length + ") does not equal the number of theta elements (" + thetaMatrix[0].length + ").");
+        else if(xMatrix[0].length != thetaVector.length) {
+            System.out.println("Check your data: the number of x elements (" + xMatrix[0].length + ") does not equal the number of theta elements (" + thetaVector.length + ").");
             exit(0);
         }
         else {
             System.out.println("Data OK.");
-            for(int thetaCount = 0; thetaCount < thetaMatrix.length; thetaCount++) {
-                System.out.println("Working on theta vector " + (thetaCount + 1));
-                System.out.println("Calculating the number of features (m)...");
-                int m = xMatrix[thetaCount].length;
-                System.out.println("The number of features is: " + m);
-                System.out.println("Calculating the hypotheses (h\u019F(x) = \u019F\u2080X\u2080 + \u019F\u2081X\u2081 + ... + \u019F\u2099X\u2099)...");
-                // Create a vector to hold each element in a theta matrix column
-                Double[] thetaVector = new Double[thetaMatrix[thetaCount].length];
+            boolean convergenceFlag = false;
+            while(convergenceFlag == false) {
+                System.out.println("Calculating the number of examples (m)...");
+                int m = xMatrix[0].length;
+                System.out.println("The number of examples is " + m);
+                System.out.println("Calculating the hypotheses: (h\u019F(x) = \u019F\u2080X\u2080 + \u019F\u2081X\u2081 + ... + \u019F\u2099X\u2099)...");
                 // Create a vector to hold each element in an x matrix column
-                Double[] xVector = new Double[xMatrix[thetaCount].length];
+                Double[] xVector = new Double[xMatrix[0].length];
                 // Create a matrix to hold all the h values per theta and x vector (used to compute sum of squares for each vector)
-                Double[][] h = new Double[thetaMatrix.length][xMatrix.length];
-                Double[][] residual = new Double[thetaMatrix.length][xMatrix.length];
-                Double[][] squaredResidualForCF = new Double[thetaMatrix.length][xMatrix.length];
-                Double[] sumOfSquares = new Double[thetaMatrix.length];
+                Double[] h = new Double[yVector.length];
+                Double[] residual = new Double[yVector.length];
+                Double[] squaredResidualForCF = new Double[yVector.length];
+                Double sumOfSquares = 0.0;
                 Double J = 0.0;
-                // Must use this instead of Math.pow to avoid "lossy conversion from double to int" error 
-                Double[] adjustedResidualForGD = new Double[xMatrix.length * xMatrix.length];
-                System.out.println("xMatrix length = " + xMatrix.length + " and adjustedResidualForGD length = " + adjustedResidualForGD.length);
+                // Must use this instead of Math.pow to avoid "lossy conversion from double to int" error
+                Double[] adjustedResidualForGD = new Double[yVector.length];
+                Double[] sumOfAdjustedResiduals = new Double[yVector.length];
                 Double alpha = 0.0;
-                Double[] tempThetaVector = new Double[thetaMatrix[thetaCount].length];
+                Double[] tempThetaVector = new Double[thetaVector.length];
                 /**
                  * To get h, you need to apply each theta vector to each vector of x.
                  * The number of theta vectors controls the outer loop.
@@ -115,15 +111,13 @@ public class MLAlgorithmsInJava {
                 for(int i = 0; i < xMatrix.length; i++) {
                     // Create the vectors for matrix multiplication
                     xVector = xMatrix[i];
-                    thetaVector = thetaMatrix[thetaCount];
-
                     // Apply the hypothesis function. If it fails, it wil return null and the application will exit.
-                    if((h[thetaCount][i] = LinearRegression.Hypothesis(thetaVector, xVector)) == null) {
+                    if((h[i] = LinearRegression.Hypothesis(thetaVector, xVector)) == null) {
                         exit(0);
                     }
                     else {
                         // My fun code to print out each hypothesis function! 
-                        System.out.printf("h\u019F(x[%d]) = %f or ", (i + 1), h[thetaCount][i]);
+                        System.out.printf("h\u019F(x[%d]) = %f or ", (i + 1), h[i]);
                         for(int k = 0; k < xVector.length; k++) {
                             System.out.printf("(%f * %f)", thetaVector[k], xVector[k]);
                             System.out.print(k == (xVector.length - 1) ? "" : " + ");
@@ -132,40 +126,48 @@ public class MLAlgorithmsInJava {
                     }
                 }
                 System.out.println("Calculating the residuals of h\u019F(x) - y...");
-                for(int i = 0; i < xMatrix.length; i++) {
-                    residual[thetaCount][i] = h[thetaCount][i] - yVector[i];
-                    System.out.printf("h\u019F(x)[%d] - y[%d] = %f or %f - %f\n", i + 1, i + 1, residual[thetaCount][i], h[thetaCount][i], yVector[i]);
+                for(int i = 0; i < yVector.length; i++) {
+                    residual[i] = h[i] - yVector[i];
+                    System.out.printf("h\u019F(x)[%d] - y[%d] = %f or %f - %f\n", i + 1, i + 1, residual[i], h[i], yVector[i]);
                 }
                 System.out.println("Calculating the squares of the residuals of h\u019F(x) - y...");
-                for(int i = 0; i < xMatrix.length; i++) {
-                    squaredResidualForCF[thetaCount][i] = Math.pow((h[thetaCount][i] - yVector[i]), 2);
-                    System.out.printf("(h\u019F(x)[%d] - y[%d])\u00B2 = %f or %f\u00B2\n", i + 1, i + 1, squaredResidualForCF[thetaCount][i], residual[thetaCount][i]);
+                for(int i = 0; i < yVector.length; i++) {
+                    squaredResidualForCF[i] = Math.pow((h[i] - yVector[i]), 2);
+                    System.out.printf("(h\u019F(x)[%d] - y[%d])\u00B2 = %f or %f\u00B2\n", i + 1, i + 1, squaredResidualForCF[i], residual[i]);
                 }
                 System.out.println("Calculating the sum of squares of the residuals of h\u019F(x) - y...");
-                System.out.print("The sum of squares for theta vector " + (thetaCount + 1) + " = ");
-                // Initialize sumOfSquares to prevent NullPointerException when using the += assignment operator
-                sumOfSquares[thetaCount] = 0.0;
-                for(int i = 0; i < xMatrix.length; i++) {
-                    sumOfSquares[thetaCount] += squaredResidualForCF[thetaCount][i];
-                    System.out.printf("%f", squaredResidualForCF[thetaCount][i]);
-                    System.out.print(i == (squaredResidualForCF[thetaCount].length - 1) ? " or " : " + ");
+                System.out.print("The sum of squares = ");
+                for(int i = 0; i < yVector.length; i++) {
+                    sumOfSquares += squaredResidualForCF[i];
+                    System.out.printf("%f", squaredResidualForCF[i]);
+                    System.out.print(i == (squaredResidualForCF.length - 1) ? " or " : " + ");
                 }
-                System.out.println(sumOfSquares[thetaCount]);
-                System.out.println("Calculating the cost function J(\u019F\u2080, \u019F\u2081...\u019F\u2099)) = (1 / (2 * m)) * (\u2211((h(x\u2099) - y\u2099)\u00B2)) for theta vector " + (thetaCount + 1) + "...");
-                J = (1.0 / (2.0 * m)) * sumOfSquares[thetaCount];
-                System.out.printf("J = %f or (1 / (2 * %d)) * %f\n", J, m, sumOfSquares[thetaCount]);
+                System.out.println(sumOfSquares);
+                System.out.println("Calculating the cost function J(\u019F\u2080, \u019F\u2081...\u019F\u2099)) = (1 / (2 * m)) * (\u2211((h(x\u2099) - y\u2099)\u00B2))...");
+                J = (1.0 / (2.0 * (m + 1))) * sumOfSquares;
+                System.out.printf("J = %f or (1 / (2 * %d)) * %f\n", J, m + 1, sumOfSquares);
                 System.out.println("Performing gradient descent...");
-                System.out.println("Adjusting original residuals for gradient descent (h\u019F(x) - y)) * x...");
-                int count = 0;
+                System.out.println("Adjusting original residuals: (h\u019F(x) - y)) * x...");
                 for(int i = 0; i < xMatrix.length; i++) {
+                    // Initialize adjustedResidualForGD to prevent NullPointerException when using the += assignment operator
+                    adjustedResidualForGD[i] = 0.0;
                     for(int j = 0; j < xMatrix[i].length; j++) {
-                        adjustedResidualForGD[count] = residual[thetaCount][i] * xMatrix[i][j];
-                        System.out.printf("h\u019F(x) - y (%f) * x[i][j] (%f) = %f | %f\n", residual[thetaCount][i], xMatrix[i][j], residual[thetaCount][i] * xMatrix[i][j], adjustedResidualForGD[count]);
+                        adjustedResidualForGD[i] += residual[i] * xMatrix[i][j];
+                        System.out.printf("h\u019F(x)[%d] - y[%d] (%f) * x[%d][%d] (%f) = %f (%f)\n", i, i, residual[i], i, j, xMatrix[i][j], (residual[i] * xMatrix[i][j]), adjustedResidualForGD[i]);
                     }
-                    count++;
+                }
+                System.out.println("Summing up adjusted original residuals for each x: (h\u019F(x) - y)) * x...");
+                // Initialize sumOfAdjustedResiduals to prevent NullPointerException when using the += assignment operator
+                // sumOfAdjustedResiduals = 0.0;
+                for(int i = 0; i < xMatrix.length; i++) {
+                    if(sumOfAdjustedResiduals[i] == null) {
+                        sumOfAdjustedResiduals[i] = 0.0;
+                    }
+                    sumOfAdjustedResiduals[i] += adjustedResidualForGD[i];
                 }
                 
                 System.out.println();
+                convergenceFlag = true;
             }
             /*
             System.out.println("\nCalculating the cost [J(\u019F\u2080, \u019F\u2081...\u019F\u2099)) = 1 / (2 * m) * (\u2211((h(x\u2099) - y\u2099)\u00B2))]...\n");
